9dcfcd23b89e2ee29c381ea70845e932a23cd6a9
flinney <77643261+Flinney@users.noreply.github.com>
2021-08-18 11:11:09 -0500

    Caesar notes

--- src/caesar.js
+++ src/caesar.js
@@ -5,23 +5,31 @@
 
 const caesarModule = (function () {
   // you can add any code you want within this function scope
+  // Create alpha string for reference
   const alpha = "abcdefghijklmnopqrstuvwxyz"
 
   function caesar(input, shift, encode = true) {
     // your solution code here
+    // Checking for proper shift amount with early return, then create answer string
     if (shift === 0 || shift < -25 || shift > 25) {
       return false
     }
     let answerStr = ""
+    // If decoding, flip shift value in opposite direction.
     if (!encode) {
       shift *= -1
     }
+    // Loop over indices of input string
     for (let i = 0; i < input.length; i++) {
+      // Force lower case on letters, then find idx of letter on alpha str.
+      // If char is not a letter, pushes char to answer str.
       const currentIdx = alpha.indexOf(input[i].toLowerCase())
       if (currentIdx === -1) {
         answerStr += input[i]
         continue
       }
+      // Add shift value to idx to get the desired idx.
+      // If shift pushes idx outside of range, subtract or add 26 to wrap around to opposite end.
       const targetIdx = currentIdx + shift
       if (targetIdx > 25) {
         answerStr += alpha[targetIdx - 26]
